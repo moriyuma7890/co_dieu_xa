310526461d59e692c5883be8186ddf6fa50de413
moriyuma7890 <moriyuma7890@github.com>
2024-09-02 18:05:59 +0700

    init

---
+++ m-a-n-h-u-a-t-o-p-o-r-g.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         m-a-n-h-u-a-t-o-p-o-r-g
+// @namespace    https://github.com/moriyuma7890/co_dieu_xa/raw/main/m-a-n-h-u-a-t-o-p-o-r-g.user.js
+// @version      2024.09.02
+// @description  down m-a-n-h-u-a-t-o-p-o-r-g
+// @author       You
+// @match        https://manhuatop.org/*/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=manhuatop.org
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.48/dist/zip.min.js
+// @grant        none
+// @downloadURL  https://github.com/moriyuma7890/co_dieu_xa/raw/main/m-a-n-h-u-a-t-o-p-o-r-g.user.js
+// @updateURL    https://github.com/moriyuma7890/co_dieu_xa/raw/main/m-a-n-h-u-a-t-o-p-o-r-g.meta.js
+// ==/UserScript==
